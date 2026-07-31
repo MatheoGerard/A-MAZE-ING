9e772eba9b9651d64391656419b42bca8099a65d
Denis Monseur <dmonseur@student.42belgium.be>
2026-07-31 16:59:35 +0200

    color palette

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -17,14 +17,14 @@ console = Console()
 
 def title_print() -> None:
     title: str = r"""
- ______                     ______  ________   ____            ______   __  __  ____      
-/\  _  \            /'\_/`\/\  _  \/\_____  \ /\  _`\         /\__  _\ /\ \/\ \/\  _`\    
-\ \ \L\ \          /\      \ \ \L\ \/____//'/'\ \ \L\_\       \/_/\ \/ \ \ `\\ \ \ \L\_\  
- \ \  __ \  _______\ \ \__\ \ \  __ \   //'/'  \ \  _\L   _______\ \ \  \ \ , ` \ \ \L_L  
+ ______                     ______  ________   ____            ______   __  __  ____
+/\  _  \            /'\_/`\/\  _  \/\_____  \ /\  _`\         /\__  _\ /\ \/\ \/\  _`\
+\ \ \L\ \          /\      \ \ \L\ \/____//'/'\ \ \L\_\       \/_/\ \/ \ \ `\\ \ \ \L\_\
+ \ \  __ \  _______\ \ \__\ \ \  __ \   //'/'  \ \  _\L   _______\ \ \  \ \ , ` \ \ \L_L
   \ \ \/\ \/\______\\ \ \_/\ \ \ \/\ \ //'/'___ \ \ \L\ \/\______\\_\ \__\ \ \`\ \ \ \/, \
    \ \_\ \_\/______/ \ \_\\ \_\ \_\ \_\/\_______\\ \____/\/______//\_____\\ \_\ \_\ \____/
-    \/_/\/_/          \/_/ \/_/\/_/\/_/\/_______/ \/___/          \/_____/ \/_/\/_/\/___/ 
-                                                                                          
+    \/_/\/_/          \/_/ \/_/\/_/\/_/\/_______/ \/___/          \/_____/ \/_/\/_/\/___/
+
 """
     console.print(Panel(title, expand=False, border_style="yellow"))
 
@@ -171,7 +171,15 @@ def init_lab(
 
 def loop_gameplay() -> None:
     color_set: list[str] = [
-        "dark_red-gold1-dark_orange3-orange_red1-bright_red-red3-white-red1",
+
+        "red-orange1-gold1-yellow1-chartreuse1-green1-cyan1-blue1",
+        "grey15-grey35-grey58-grey78-white-steel_blue1-orchid1-deep_pink2",
+        "deep_sky_blue1-steel_blue1-cyan1-medium_spring_green-chartreuse1-gold1-dark_orange-red1",
+        "purple-blue_violet-royal_blue1-cornflower_blue-steel_blue1-sky_blue1-light_sky_blue1-white",
+        "deep_pink2-hot_pink-orchid1-plum1-medium_orchid1-violet-thistle1-white",
+        "dark_red-red3-orange3-gold3-yellow3-chartreuse3-green3-dark_green",
+        "grey3-grey23-grey42-grey62-grey82-grey93-white-black",
+        "navy_blue-deep_sky_blue4-deep_sky_blue1-cyan1-medium_spring_green-green1-chartreuse1-yellow1",
         # "purple-orchid-blue",
         # "bright_red-grey0-bright_blue",
         # "grey3-grey46-grey85",
